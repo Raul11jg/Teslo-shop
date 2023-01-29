72eda16cbe2d76399826213f6d257e0478054aed
Raul11jg <raul11jg@gmail.com>
2023-01-29 14:15:18 +0100

    SSProps to get product by slug

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -18,7 +18,7 @@ export const ProductCard: FC<Props> = ({ product }) => {
   return (
     <span onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
       <Card>
-        <NextLink href="/product/slug" passHref prefetch={false}>
+        <NextLink href={`/product/${product.slug}`} passHref prefetch={false}>
           <Link>
             <CardActionArea>
               <CardMedia component="img" className="fadeIn" image={productImage} alt={product.title} onLoad={() => setIsImageLoaded(true)} />
--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,12 +1,20 @@
-import { Box, Button, Chip, Grid, Typography } from '@mui/material';
+import { GetServerSideProps } from 'next';
+import { Box, Button, Grid, Typography } from '@mui/material';
+import { NextPage } from 'next';
 import { ShopLayout } from '../../components/layouts';
 import { ProductSlideshow, SizeSelector } from '../../components/products';
 import { ItemCounter } from '../../components/ui';
-import { initialData } from '../../database/products';
+import { IProduct } from '../../interfaces';
+import { dbProducts } from '../../database';
 
-const product = initialData.products[0];
+interface Props {
+  product: IProduct;
+}
 
-const ProductPage = () => {
+const ProductPage: NextPage<Props> = ({ product }) => {
+  //const router = useRouter();
+  //const { products: product, isLoading } = useProducts(`/products/${router.query.slug}`);
+
   return (
     <ShopLayout title={'ABC'} pageDescription={''}>
       <Grid container spacing={3}>
@@ -42,6 +50,29 @@ const ProductPage = () => {
       </Grid>
     </ShopLayout>
   );
+};
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { slug } = params as { slug: string };
+
+  const product = await dbProducts.getProductBySlug(slug);
+
+  if (!product)
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+
+  return {
+    props: {
+      product,
+    },
+  };
 };
 
 export default ProductPage;
