5d8601a53d882a0ffce10f92e040144ddfcdbcf4
Raul11jg <raul11jg@gmail.com>
2023-02-01 17:52:57 +0100

    jwt token

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { db } from '../../../database';
 import { User } from '../../../models';
 import bcrypt from 'bcryptjs';
+import { jwt } from '../../../utils';
 
 type Data =
   | {
@@ -43,10 +44,12 @@ const loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     return res.status(400).json({ message: 'Invalid credentials or email' });
   }
 
-  const { role, name } = user;
+  const { role, name, _id } = user;
+
+  const token = jwt.signToken(_id, email);
 
   return res.status(200).json({
-    token: '', //TODO jwt
+    token,
     user: {
       email,
       role,
