6d8fc8385ddb17acb6204c00b6e6851fe354f86c
Raul11jg <raul11jg@gmail.com>
2023-02-07 19:32:43 +0100

    address Cookies remove on log out

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -79,6 +79,15 @@ export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
     Cookies.remove('token');
     Cookies.remove('cart');
 
+    Cookies.remove('firstName');
+    Cookies.remove('lastName');
+    Cookies.remove('address');
+    Cookies.remove('address2');
+    Cookies.remove('postalCode');
+    Cookies.remove('city');
+    Cookies.remove('country');
+    Cookies.remove('phone');
+
     //refresh page
     router.reload();
   };
