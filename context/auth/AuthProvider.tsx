0550e7d4d87a6b4c36e725bbde50d5bc4f1a8cca
Raul11jg <raul11jg@gmail.com>
2023-02-08 17:09:28 +0100

    authProvider next

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -1,3 +1,4 @@
+import { useSession } from 'next-auth/react';
 import { FC, PropsWithChildren, useEffect, useReducer } from 'react';
 import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
@@ -20,9 +21,18 @@ export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE);
   const router = useRouter();
 
+  const { data, status } = useSession();
+
   useEffect(() => {
+    if (status === 'authenticated') {
+      console.log('data', data.user);
+      //dispatch({ type: '[Auth] - Login', payload: data.user as IUser });
+    }
+  }, [status, data]);
+
+  /*   useEffect(() => {
     checkToken();
-  }, []);
+  }, []); */
 
   const checkToken = async () => {
     if (!Cookies.get('token')) {
