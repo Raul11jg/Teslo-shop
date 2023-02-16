681246b8bb703c415e9b71c8d098aa64f767eb17
Raul11jg <raul11jg@gmail.com>
2023-02-16 12:19:44 +0100

    api admin users

--- models/User.ts
+++ models/User.ts
@@ -6,7 +6,7 @@ const userSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: { values: ['admin', 'client'], message: '{VALUE} is not a valid role', default: 'client', required: true } },
+    role: { type: String, enum: { values: ['admin', 'client', 'super-user', 'SEO'], message: '{VALUE} is not a valid role', default: 'client', required: true } },
   },
   {
     timestamps: true,
